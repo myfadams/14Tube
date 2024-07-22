a25581f5e12f3bcacee89580585f81e00e3f9ff1
myfadams <124105077+myfadams@users.noreply.github.com>
2024-07-22 23:54:47 +0000

    Update appwrite.js

--- libs/appwrite.js
+++ libs/appwrite.js
@@ -9,13 +9,6 @@ import {
 } from "react-native-appwrite";
 
 export const appwriteConfig = {
-	endpoint: "https://cloud.appwrite.io/v1",
-	Platform: "com.jsm.learnReact",
-	projectId: "666ec1c80026ad9445c9",
-	databaseId: "666ec490003202b8490e",
-	userCollectionId: "666ec4d2003732e5c7f2",
-	videosCollectionId: "666ec50b002a86e2112b",
-	storageId: "666ecb92003dd5320fb8",
 };
 
 const client = new Client();
